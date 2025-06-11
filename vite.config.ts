0ddd5f04adcee7064643cdc2aee2dc8a5cb6ea09
Marius Roelly <m.roelly@opticod.fr>
2025-06-11 10:35:20 +0200

    Add deploy pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/nk-download-page/",
+  build: {
+    outDir: 'dist',
+    sourcemap: false, // Optional: disable sourcemaps for smaller build
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
